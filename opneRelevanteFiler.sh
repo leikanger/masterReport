1d5f468eb05fa39e64c233450c66c4621e7b47b5
Per R. Leikanger <leikange@gmail.com>
2012-01-20 16:32:58 +0100

    Har nesten skrevet ferdig Background Theory: Biological Neural Systems.

--- opneRelevanteFiler.sh
+++ opneRelevanteFiler.sh
@@ -1,1 +1,1 @@
-vim -p opneRelevanteFiler.sh report.tex introduction.tex biologicalNeuralSystems.tex artificialNeuralSystms.tex #NIM-simulation.tex novelModel.tex 
+vim -p opneRelevanteFiler.sh report.tex introduction.tex biologicalNeuralSystems.tex artificialNeuralSystms.tex              appendixSynPlast.tex bibliografi.bib 

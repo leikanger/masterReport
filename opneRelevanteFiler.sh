5d9191e95ae019b209342d204d15298d3b1df7ee
Per R. Leikanger <leikange@gmail.com>
2012-02-15 17:32:34 +0100

    jaja. Tar helg i femunden..

--- opneRelevanteFiler.sh
+++ opneRelevanteFiler.sh
@@ -1,1 +1,13 @@
-vim -p report.tex introduction.tex biologicalNeuralSystems.tex artificialNeuralSystms.tex  theNovelModel.tex generalDesign.tex KMdesign.tex           appendixSynPlast.tex appendix.tex bibliografi.bib TODO
+vim -p TODO report.tex introduction.tex biologicalNeuralSystems.tex artificialNeuralSystms.tex  theNovelModel.tex generalDesign.tex KMdesign.tex           appendixSynPlast.tex appendix.tex bibliografi.bib
+#appendixSynPlast.tex
+#appendix.tex
+#artificialNeuralSystms.tex
+#biologicalNeuralSystems.tex
+#generalDesign.tex
+#introduction.tex
+#KMdesign.tex
+#novelModel.tex
+#PLAN?
+#report.tex
+#theNovelModel.tex
+#TODO
